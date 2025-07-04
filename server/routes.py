5ba802dfbe2b82c643d1d44edaee1c196205c291
gadm21 <g.gad@nu.edu.eg>
2025-07-04 15:53:20 -0400

    lj

--- server/routes.py
+++ server/routes.py
@@ -92,7 +92,8 @@ from .auth import (
     authenticate_user, 
     create_access_token,
     get_current_user, 
-    ACCESS_TOKEN_EXPIRE_MINUTES
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+    verify_password
 )
 
 # Import schemas
